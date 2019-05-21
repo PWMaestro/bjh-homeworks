6620ef2728ad039a2d5515c55cf5d9767f00cee5
Artiom Sitsko <AC3600@yandex.by>
2019-05-21 08:26:15 +0300

    JS features. Task 2. Advanced level 1

--- 01-js-features/cookie-clicker/task.js
+++ 01-js-features/cookie-clicker/task.js
@@ -1,25 +1,36 @@
-const cookie = document.getElementById('cookie');
-
-function changeCookieSize(width) {
-	switch(width) {
-		case 200:
-			cookie.width = 300;
-			break;
-		case 300:
-			cookie.width = 200;
-			break;
-	}
-}
-
 function startCount() {
+	function changeCookieSize(width) {
+		switch(width) {
+			case 200:
+				cookie.width = 300;
+				break;
+			case 300:
+				cookie.width = 200;
+				break;
+		}
+	}
+
+	function calculateClickSpeed() {
+		const now = Date.now();
+		const period = (now - lastClickDate) / 1000;
+		const frequency = 1 / period;
+		lastClickDate = now;
+
+		return frequency.toFixed(2);
+	}
+
 	const visibleCounter = document.getElementById('clicker__counter');
+	const visibleSpeed = document.getElementById('clicker__speed');
+	let lastClickDate = Date.now();
 	let clickCounter = 0;
 
 	return () => {
 		clickCounter++;
 		visibleCounter.textContent = clickCounter;
 		changeCookieSize(cookie.width);
+		visibleSpeed.textContent = calculateClickSpeed();
 	}
 }
 
+const cookie = document.getElementById('cookie');
 cookie.onclick = startCount();
