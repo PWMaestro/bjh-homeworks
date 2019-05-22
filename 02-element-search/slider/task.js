c419343d2f3306aa2d29692692f7c8bb7e95fba7
Artiom Sitsko <AC3600@yandex.by>
2019-05-22 22:21:22 +0300

    Element search. Task 3. Advanced level 1

--- 02-element-search/slider/task.js
+++ 02-element-search/slider/task.js
@@ -2,24 +2,64 @@ const nextBtn = Array.from(document.getElementsByClassName('slider__arrow_next')
 	previousBtn = Array.from(document.getElementsByClassName('slider__arrow_prev'))[0];
 
 nextBtn.onclick = () => {
-	const activeImg = document.querySelector('.slider__items  .slider__item_active');
+	const activeImg = document.querySelector('.slider__items  .slider__item_active'),
+		activeDot = document.querySelector('.slider__dots .slider__dot_active');
+
 	activeImg.classList.remove('slider__item_active');
+	activeDot.classList.remove('slider__dot_active');
+
 	if (activeImg.nextElementSibling) {
 		activeImg.nextElementSibling.classList.add('slider__item_active');
+		activeDot.nextElementSibling.classList.add('slider__dot_active');
 	} else {
-		document.querySelector('.slider__items  .slider__item').classList.add('slider__item_active');
+		document.querySelector('.slider__items  .slider__item')
+			.classList
+			.add('slider__item_active');
+
+		document.querySelector('.slider__dots  .slider__dot')
+			.classList
+			.add('slider__dot_active');
 	}
 }
 
 previousBtn.onclick = () => {
-	const activeImg = document.querySelector('.slider__items  .slider__item_active');
+	const activeImg = document.querySelector('.slider__items  .slider__item_active'),
+		activeDot = document.querySelector('.slider__dots .slider__dot_active');
+
 	activeImg.classList.remove('slider__item_active');
+	activeDot.classList.remove('slider__dot_active');
+
 	if (activeImg.previousElementSibling) {
 		activeImg.previousElementSibling.classList.add('slider__item_active');
+		activeDot.previousElementSibling.classList.add('slider__dot_active');
 	} else {
 		Array.from(document.querySelectorAll('.slider__items  .slider__item'))
 			.pop()
 			.classList
 			.add('slider__item_active');
+
+		Array.from(document.querySelectorAll('.slider__dots  .slider__dot'))
+			.pop()
+			.classList
+			.add('slider__dot_active');
 	}
 }
+
+const dots = Array.from(document.querySelectorAll('.slider__dots .slider__dot')),
+	images = Array.from(document.querySelectorAll('.slider__items .slider__item'));
+
+dots.forEach((item, i) => {
+	item.onclick = () => {
+		document.querySelector('.slider__dots .slider__dot_active')
+			.classList
+			.remove('slider__dot_active');
+
+		item.classList.add('slider__dot_active');
+
+		document.querySelector('.slider__items  .slider__item_active')
+			.classList
+			.remove('slider__item_active');
+
+		images[i].classList.add('slider__item_active');
+	}
+});
