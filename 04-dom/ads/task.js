d586799c0d293fda688dd6933216e57545e4a10d
Artiom Sitsko <AC3600@yandex.by>
2019-05-29 10:00:51 +0300

    DOM. Task 2. Advanced level 1.2

--- 04-dom/ads/task.js
+++ 04-dom/ads/task.js
@@ -1,15 +1,27 @@
 function rotate(roll) {
 	const adList = Array.from(roll.getElementsByClassName('rotator__case'));
 	let activeAdIndex = adList.indexOf(roll.querySelector('.rotator__case_active'));
+	let rotatorId = 0;
 
-	return setInterval(() => {
+	function next() {
 		adList[activeAdIndex].classList.remove('rotator__case_active');
 		if (++activeAdIndex === adList.length) {
 			activeAdIndex = 0;
 		}
 		adList[activeAdIndex].classList.add('rotator__case_active');
 		adList[activeAdIndex].style.color = adList[activeAdIndex].dataset.color;
-	}, 1000);
+		rotatorId = setTimeout(next, adList[activeAdIndex].dataset.speed);
+	}
+
+	return () => {
+		if (rotatorId === 0) {
+			next();
+			return rotatorId;
+		} else {
+			return rotatorId;
+		}
+	}
 }
 
 const rotatorId = rotate(document.querySelector('.rotator'));
+rotatorId();// returns Id to clear current timeout
