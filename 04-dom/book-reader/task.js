10efac404dedb81d5d172bdcdddf17ca3309c7a6
Artiom Sitsko <AC3600@yandex.by>
2019-05-29 17:47:04 +0300

    DOM. Task 3. Advanced level 1. Text color controller added

--- 04-dom/book-reader/task.js
+++ 04-dom/book-reader/task.js
@@ -5,10 +5,83 @@ class Controls {
 			value: book.querySelector('.book__control_font-size'),
 			elements: Array.from(book.getElementsByClassName('font-size'))
 		}
+		this.textColorPanel = {
+			value: book.querySelector('.book__control_color'),
+			elements: Array.from(book.getElementsByClassName('color'))
+		}
 
 		this.registerEvents();
 	}
 
+	registerEvents() {
+		this.fontSizePanel.elements.forEach((item, i) => { //addEventListener on font-size controller
+			item.addEventListener('click', (event) => {
+				event.preventDefault();
+				this.fontSizePanel
+					.value
+					.querySelector('.font-size_active')
+					.classList
+					.remove('font-size_active');
+
+				this.fontSizePanel
+					.elements[i]
+					.classList
+					.add('font-size_active');
+	
+				this.setScaledFontSize(item);
+			});
+
+		});
+
+		this.textColorPanel.elements.forEach((item, i) => { //addEventListener on text-color controller
+			item.addEventListener('click', (event) => {
+				event.preventDefault();
+				this.textColorPanel
+					.value
+					.querySelector('.color_active')
+					.classList
+					.remove('color_active');
+
+				this.textColorPanel
+					.elements[i]
+					.classList
+					.add('color_active');
+	
+				this.setTextColor(item);
+			});
+
+		});
+	}
+
+	setScaledFontSize(element) {
+		this.book.classList.forEach((item) => {
+			if ( (item !== 'book') && item.includes('book_fs-') ) {
+				this.book.classList.remove(item);
+			}
+		});
+
+		if (element.classList.contains('font-size_small')) {
+			this.book.classList.add(`book_fs-${element.dataset.size}`);
+		} else if (element.classList.contains('font-size_big')) {
+			this.book.classList.add(`book_fs-${element.dataset.size}`);
+		}
+	}
+
+	setTextColor(element) {
+		this.book.classList.forEach((item) => {
+			if ( (item !== 'book') && item.includes('book_color-') ) {
+				this.book.classList.remove(item);
+			}
+		});
+
+		if (element.classList.contains('color_gray')) {
+			this.book.classList.add(`book_color-${element.dataset.color}`);
+		} else if (element.classList.contains('color_whitesmoke')) {
+			this.book.classList.add(`book_color-${element.dataset.color}`);
+		}
+	}
+
+/*
 	registerEvents() {
 		this.fontSizePanel.elements.forEach((item, i) => {
 			item.addEventListener('click', (event) => {
@@ -43,6 +116,7 @@ class Controls {
 			this.book.classList.add(`book_fs-${element.dataset.size}`);
 		}
 	}
+	*/
 }
 
 new Controls(document.getElementById('book'));
