99e87da091ee0e693cf4a76a059938a09e5820af
Artiom Sitsko <AC3600@yandex.by>
2019-06-07 15:20:06 +0300

    HTML Forms. Task 2. Advanced levels

--- 05-html-forms/interests/task.js
+++ 05-html-forms/interests/task.js
@@ -1,17 +1,42 @@
 const interests = Array.from(document.getElementsByClassName('interest__check'));
-console.log(interests);
 
 interests.forEach(element => {
 	element.addEventListener('change', event => {
-		const innerInterest = Array.from(
-			event.target
-				.closest('.interest')
-				.getElementsByClassName('interest__check')
-		);
-		if (event.target.checked) {
-			innerInterest.forEach(element => {element.checked = true});
-		} else {
-			innerInterest.forEach(element => {element.checked = false});
-		}
+		checkCurrent(event.target);
+		checkParent(event.target);
 	});
 });
+
+function checkCurrent(element) {
+	const innerInterest = Array.from(
+		element.closest('.interest')
+			.getElementsByClassName('interest__check')
+	);
+	if (element.checked) {
+		innerInterest.forEach(element => {element.checked = true});
+	} else {
+		innerInterest.forEach(element => {element.checked = false});
+	}
+}
+
+function checkParent(element) {
+	const parent = element.closest('ul'),
+		grandPa = parent.closest('li');
+
+	if (grandPa) {
+		const checked = Array.from(parent.querySelectorAll('.interest__check:checked')),
+			all = Array.from(parent.getElementsByClassName('interest__check')),
+			aboveEl = grandPa.querySelector('input.interest__check');
+
+		if (checked.length === 0) {
+			aboveEl.indeterminate = false;
+			aboveEl.checked = false;
+		} else if (checked.length === all.length) {
+			aboveEl.indeterminate = false;
+			aboveEl.checked = true;
+		} else {
+			aboveEl.indeterminate = true;
+		}
+		checkParent(aboveEl);
+	}
+}
