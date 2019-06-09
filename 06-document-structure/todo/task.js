061d774258eb161834e3150ca83dc8cbf5e83079
Artiom Sitsko <AC3600@yandex.by>
2019-06-09 19:13:06 +0300

    Doc. Structure. Task 2. Advanced level

--- 06-document-structure/todo/task.js
+++ 06-document-structure/todo/task.js
@@ -5,37 +5,129 @@ class ToDoList {
 		this.list = container.querySelector('#tasks__list');
 
 		this.registerEvents();
+
+		Storage.getFrom().forEach( item => {
+			const task = this.wrapUp(item);
+			this.show(task);
+		});
 	}
 
 	registerEvents() {
 		this.addButton.addEventListener('click', event => {
 			event.preventDefault();
-			!this.input.value.trim() || this.create();
+			const input = this.input.value.trim();
+			!input || this.createTask( input );
 		});
 	}
 
-	create() {
+	wrapUp( {text, index} ) {
 		const element = document.createElement('div');
 		element.classList.add('task');
+		element.dataset.index = index;
 		element.innerHTML = `
 			<div class="task__title">
-				${this.input.value.trim()}
+				${text}
 			</div>
 			<a href="#" class="task__remove">&times;</a>
 		`;
+		this.addEvent(
+			{
+				item: element.querySelector('.task__remove'),
+				index
+			}
+		);
+
+		return element;
+	}
+
+	createTask(text) {
+		const index = Storage.getCurrentIndex();
+		const element = this.wrapUp( {text, index} );
+		
+		Storage.add(text);
+
+		this.show(element);
 		this.input.value = '';
+	}
 
-		element.getElementsByClassName('task__remove')
-			.item(0)
-			.addEventListener('click', event => {
-				event.preventDefault();
-				event.target
-					.closest('.task')
-					.remove();
-			});
+	addEvent( {item, index} ) {
+		item.addEventListener('click', event => {
+			event.preventDefault();
+			Storage.remove(index);
+			this.remove( event.target.closest('.task') );
+		});		
+	}
 
-		this.list.appendChild(element);
+	show(item) {
+		this.list.appendChild(item);
+	}
+
+	remove(item) {
+		item.remove();
 	}
 }
 
+
+
+
+
+class LocalStorage {
+	constructor() {
+		if ( !localStorage.getItem('counter') ) {
+			localStorage.setItem( 'counter', JSON.stringify({ index: 0 }) );
+			localStorage.setItem( 'box', JSON.stringify({ }) );
+		}
+	}
+
+	add(item) {
+		const box = JSON.parse( localStorage.getItem('box') );
+
+		box[this.getCurrentIndex()] = item;
+		localStorage.setItem( 'box', JSON.stringify(box) );
+		this.nextIndex();
+	}
+
+	getFrom() {
+		const box = JSON.parse( localStorage.getItem('box') ),
+			output = [];
+
+		for (let key in box) {
+			const obj = {
+				text: box[key],
+				index: key
+			}
+			output.push(obj);
+		}
+
+		return output;
+	}
+
+	getCurrentIndex() {
+		return JSON.parse( localStorage.getItem('counter') ).index;
+	}
+
+	nextIndex() {
+		const newIndex = JSON.parse( localStorage.getItem('counter') );
+		newIndex.index++;
+		localStorage.setItem( 'counter', JSON.stringify(newIndex) );
+	}
+
+	remove(index) {
+		const box = JSON.parse( localStorage.getItem('box') );
+		for (let key in box) {
+			// console.log(key);
+			// console.log(index);
+			if (key === index) {
+				delete box[key];
+			}
+		}
+		localStorage.setItem( 'box', JSON.stringify(box) );
+	}
+}
+
+
+
+
+
+const Storage = new LocalStorage();
 new ToDoList( document.getElementById('tasks') );
